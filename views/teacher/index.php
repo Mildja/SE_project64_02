4d3bf78ba35117797c502d2bfcbf33079d1cf4ac
mild <veeraya47@gmail.com>
2022-03-20 13:38:51 +0700

    up

--- views/teacher/index.php
+++ views/teacher/index.php
@@ -9,39 +9,44 @@
 <body bgcolor="#DDDDDD">
   <br><br><br><br><br><br>
 
+
   <div class="container">
+    <div class="row ">
+      <div class='col-sm-16 m-auto'>
+        <div class="card text-center">
+
+
+          <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="true" href="#">ใบคำร้องทั้งหมด</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">ใบคำร้องที่รอการอนุมัติ</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">ใบคำร้องที่อนุมัติแล้ว</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">ใบคำร้องที่ไม่อนุมัติ</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+          </div>
+
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
     
-  </div>
-  <div class="container">
-    <div class="row justify-content-center">
-    </div class='col-md-10 ms-md-auto'>
-    <div class="card text-center">
-
-
-      <div class="card-header">
-        <ul class="nav nav-tabs card-header-tabs">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="true" href="#">Active</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-      </div>
-
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-
-      </div>
+    
     </div>
-  </div>
-  </div>
-  </div>
+ </div>
 </body>
 
 <html>
