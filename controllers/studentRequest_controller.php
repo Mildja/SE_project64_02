d4d60d57e1f827ab85516e8b46a88f11db4de16a
Namphueng <namphueng.k@ku.th>
2022-03-22 04:53:44 +0700

    Update studentRequest_controller.php

--- controllers/studentRequest_controller.php
+++ controllers/studentRequest_controller.php
@@ -5,5 +5,12 @@
         echo "haha";
         require_once('views/studentRequest/index.php');
     }
-  
+   
+
+    public function detailRequest(){
+         echo "123";
+        $studentRequestList=studentRequest::getAll();
+       
+       require_once('./views/studentRequest/detailRequest.php');
+   }
 }?>
