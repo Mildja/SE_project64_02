d74c46f5d16233239701cb5d32f15e6086677018
Namphueng <namphueng.k@ku.th>
2022-03-22 04:53:37 +0700

    Update studentModel.php

--- models/studentModel.php
+++ models/studentModel.php
@@ -4,48 +4,134 @@ class studentRequest
 
 {
 
-    public $O_name,$R_type,$S_id;
+    public $S_id,
+        $O_id, $O_name, $AP_date, $R_status, $R_type, $S_fname, $S_lname,
+        $R_position, $R_sdate, $R_fdate, $O_addr, $C_fname, $C_lname, $C_email, $C_tel,
+        $D_fname, $D_lname, $D_position, $DR_path;
 
 
-
-    public function __construct($S_id,$O_name,$R_type)
-
-    {
+    public function __construct(
+        $S_id,
+        $O_id,
+        $O_name,
+        $AP_date,
+        $R_status,
+        $R_type,
+        $S_fname,
+        $S_lname,
+        $R_position,
+        $R_sdate,
+        $R_fdate,
+        $O_addr,
+        $C_fname,
+        $C_lname,
+        $C_email,
+        $C_tel,
+        $D_fname,
+        $D_lname,
+        $D_position,
+        $DR_path
+    ) {
         $this->S_id = $S_id;
+        $this->O_id = $O_id;
         $this->O_name = $O_name;
+        $this->AP_date = $AP_date;
+        $this->R_status = $R_status;
         $this->R_type = $R_type;
+        $this->S_fname = $S_fname;
+        $this->S_lname = $S_lname;
+        $this->R_position = $R_position;
+        $this->R_sdate = $R_sdate;
+        $this->R_fdate = $R_fdate;
+        $this->O_addr = $O_addr;
+        $this->C_fname = $C_fname;
+        $this->C_lname = $C_lname;
+        $this->C_email = $C_email;
+        $this->C_tel = $C_tel;
+        $this->D_fname = $D_fname;
+        $this->D_lname = $D_lname;
+        $this->D_position = $D_position;
+        $this->DR_path = $DR_path;
     }
 
 
     public static function getAll()
 
     {
-    
+
 
         $studentRequestList = [];
 
         require("connect_connection.php");
 
-        $sql = "SELECT Request.S_id, Organization.O_name,Request.R_type
-        FROM Request INNER JOIN Data_namedoc ON Request.D_id=Data_namedoc.D_id
-        INNER JOIN Organization ON Organization.O_id =Data_namedoc.O_id";
+        $sql = "SELECT Request.S_id,Organization.O_id,Organization.O_name,AP_Request.AP_date,Request.R_status,Request.R_type,
+        Student.S_fname,Student.S_lname,  Request.R_position,Request.R_sdate,Request.R_fdate,      Organization.O_addr,Colabor.C_fname,
+        Colabor.C_lname,Colabor.C_email,Colabor.C_tel,
+        Data_namedoc.D_fname,Data_namedoc.D_lname,
+        Data_namedoc.D_position,
+        Doc_Request.DR_path
+        
+        FROM Request LEFT JOIN AP_Request ON AP_Request.R_id = Request.R_id
+        INNER JOIN Colabor ON Colabor.C_id = Request.C_id
+        INNER JOIN Organization ON Colabor.O_id = Organization.O_id
+        INNER JOIN Student ON Student.S_id =Request.S_id
+        INNER JOIN Data_namedoc ON Data_namedoc.D_id = Request.D_id
+        LEFT JOIN Doc_Request ON Doc_Request.R_id = Request.R_id
+        
+        
+        ";
 
         $result = $conn->query($sql);
 
         while ($my_row = $result->fetch_assoc()) {
-            $S_id = $my_row[S_id];
-            $O_name= $my_row[O_name];
-            $R_type = $my_row[R_type];
-           
+         
 
-            $studentRequestList[] = new studentRequest($S_id,$O_name,$R_type);
+            $S_id= $my_row["S_id"];
+            $O_id= $my_row["O_id"];
+            $O_name= $my_row["O_name"];
+            $AP_date= $my_row["AP_date"];
+            $R_status= $my_row["R_status"];
+            $R_type= $my_row["R_type"];
+            $S_fname= $my_row["S_fname"];
+            $S_lname= $my_row["S_lname"];
+            $R_position= $my_row["R_position"];
+            $R_sdate= $my_row["R_sdate"];
+            $R_fdate= $my_row["R_fdate"];
+            $O_addr= $my_row["O_addr"];
+            $C_fname= $my_row["C_fname"];
+            $C_lname= $my_row["C_lname"];
+            $C_email= $my_row["C_email"];
+            $C_tel= $my_row["C_tel"];
+            $D_fname= $my_row["D_fname"];
+            $D_lname= $my_row["D_lname"];
+            $D_position= $my_row["D_position"];
+            $DR_path = $my_row["DR_path"];
 
+
+            $studentRequestList[] = new studentRequest(  $S_id,
+            $O_id,
+            $O_name,
+            $AP_date,
+            $R_status,
+            $R_type,
+            $S_fname,
+            $S_lname,
+            $R_position,
+            $R_sdate,
+            $R_fdate,
+            $O_addr,
+            $C_fname,
+            $C_lname,
+            $C_email,
+            $C_tel,
+            $D_fname,
+            $D_lname,
+            $D_position,
+            $DR_path);
         }
 
         require("connection_close.php");
 
         return $studentRequestList;
-
     }
-
 }
