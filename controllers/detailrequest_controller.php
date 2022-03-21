cea5a0f203a6e928f0a0ecd2923f299d55336e0b
Namphueng <namphueng.k@ku.th>
2022-03-22 04:53:47 +0700

    Delete detailrequest_controller.php

--- controllers/detailrequest_controller.php
+++
@@ -1,8 +0,0 @@
-<?php class detailrequestController
-{
-    public function index(){
-         $detailrequestList=detailrequest::getAll();
-        require_once('views/detailrequest/index.php');
-    }
-  
-}?>
