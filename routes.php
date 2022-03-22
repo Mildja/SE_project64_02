c66cea35ec6a4f2b40c3e612efeea1f8e1f382d2
Namphueng <namphueng.k@ku.th>
2022-03-22 23:27:39 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,49 +1,49 @@
 <?php
-$controllers = array('pages'=>['home','error'],
-'teacher'=>['index','page1'],
-'studentRequest' =>['index','detailRequest','newRequest'],
+$controllers = array(
+    'pages' => ['home', 'error'],
+    'teacher' => ['index', 'page1'],
+    'studentRequest' => ['index', 'detailRequest', 'newRequest','addRequest'],
 
 
 );
 
-function call($controller,$action){
-    require_once("./controllers/".$controller."_controller.php");
-    
-    switch($controller)
-    {
-        case "pages":                   $controller = new PagesController();
-                                        break;
-    
-                                        
-                          
-        case "teacher":                require_once("./models/teacherModel.php");                                   
-                                       $controller = new teacherController();
-                                       break;   
-                                       
-        case "studentRequest":         require_once("./models/studentModel.php");                                   
-                                       $controller = new studentRequestController();
-                                       break;  
-      
+function call($controller, $action)
+{
+    require_once("./controllers/" . $controller . "_controller.php");
+
+    switch ($controller) {
+        case "pages":
+            $controller = new PagesController();
+            break;
+
+
+
+        case "teacher":
+            require_once("./models/teacherModel.php");
+            $controller = new teacherController();
+            break;
+
+        case "studentRequest":
+            require_once("./models/studentModel.php");
+            require_once("./models/Colabor.php");
+            require_once("./models/Data_namedoc.php");
+            require_once("./models/Organization.php");
+            require_once("./models/Student.php");
+            $controller = new studentRequestController();
+            break;
     }
     $controller->{$action}();
-
 }
-if(array_key_exists($controller,$controllers)){
+if (array_key_exists($controller, $controllers)) {
 
-    if(in_array($action,$controllers[$controller]))
+    if (in_array($action, $controllers[$controller]))
 
-        call($controller,$action);
+        call($controller, $action);
 
     else
 
-        call('pages','error');
+        call('pages', 'error');
+} else {
 
+    call('pages', 'error');
 }
-
-else{
-
-    call('pages','error');
-
-}
-
-
