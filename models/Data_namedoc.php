ea4ae3b3be2680d66e235d758c5bf9ed65251108
Namphueng <namphueng.k@ku.th>
2022-03-22 23:27:22 +0700

    Update Data_namedoc.php

--- models/Data_namedoc.php
+++ models/Data_namedoc.php
@@ -7,7 +7,7 @@ class Data_namedoc
     public $D_id,$D_fname,$D_lname,$D_position,$O_id;
 
 
-    public function __construct($D_id,$D_fname,$D_lname,$D_position,$O_idr)
+    public function __construct($D_id,$D_fname,$D_lname,$D_position,$O_id)
 
     {
 
@@ -31,7 +31,7 @@ class Data_namedoc
 
         $Data_namedocList = [];
 
-        require("connect_database.php");
+        require("connect_connection.php");
 
         $sql = "SELECT * FROM Data_namedoc";
 
@@ -39,24 +39,42 @@ class Data_namedoc
 
         while ($my_row = $result->fetch_assoc()) {
 
-            $D_id = $my_row[D_id];
+            $D_id = $my_row['D_id'];
 
-            $D_fname = $my_row[D_fname];
+            $D_fname = $my_row['D_fname'];
 
-            $D_lname = $my_row[D_lname];
+            $D_lname = $my_row['D_lname'];
 
-            $D_position = $my_row[D_position];
+            $D_position = $my_row['D_position'];
 
-            $O_idr = $my_row[O_idr];
+            $O_idr = $my_row['O_id'];
 
 
-            $Data_namedocList[] = new Data_namedoc($D_id,$D_fname,$D_lname,$D_position,$O_idr);
+            $Data_namedocList[] = new Data_namedoc($D_id,$D_fname,$D_lname,$D_position,$O_id);
 
         }
 
         require("connection_close.php");
 
         return $Data_namedocList;
+    }
+
+    public static function Add($D_fname,$D_lname,$D_position)
+
+    { 
+
+       require("connect_connection.php");
+
+      
+
+       $sql = "INSERT INTO `Data_namedoc` (`D_fname`, `D_lname`, `D_position`) VALUES ('$D_fname', '$D_lname', '$D_position')";
+
+       $result = $conn->query($sql);
+
+       require("connection_close.php");
+
+       return  ;
+
     }
 
 }
