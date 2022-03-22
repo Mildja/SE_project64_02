22ee7b69896515e3a0d684226c40d6bdd9c65ee0
mild <veeraya47@gmail.com>
2022-03-23 00:04:19 +0700

    Update teacher_controller.php

--- controllers/teacher_controller.php
+++ controllers/teacher_controller.php
@@ -4,5 +4,13 @@
         $teacher_list = teacher::getAll();
         require_once('views/teacher/index.php');
     }
+    public function addAP_request()
+    {
+        $RH_ID = $_GET['RH_ID'];
+        $RH_Name = $_GET['RH_Name'];
+        $RH_Date= $_GET['RH_Date'];
+        teacher::addAP_request($AP_id,$AP_date,$AP_approve,$AP_note,$A_id,$R_id);
+        teacherController::index();
+    }
    
 }?>
