c3825f5f9131354945055b66bfa426cb1288833a
Namphueng <namphueng.k@ku.th>
2022-03-22 20:08:58 +0700

    Update detailRequest.php

--- views/studentRequest/detailRequest.php
+++ views/studentRequest/detailRequest.php
@@ -45,7 +45,7 @@
                             <p5><?php echo "สถานะ $studentRequest->R_status"; ?></p5><br/><br/><br/>
                             <p5><?php echo "$studentRequest->DR_path"; ?></p5><br/><br/><br/>
                             
-                            <a type="button" href="./views/studentRequest/index.php" class="btn btn-success">ย้อนกลับ</a>
+                            <a type="button" href="?controller=studentRequest&action=index" class="btn btn-success">ย้อนกลับ</a>
                            
                         </div>
                         
