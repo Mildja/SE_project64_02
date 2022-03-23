44a5a2eed38891dd25f37adbe85f932098fcf26a
Namphueng <namphueng.k@ku.th>
2022-03-23 14:22:03 +0700

    Update studentModel.php

--- models/studentModel.php
+++ models/studentModel.php
@@ -214,15 +214,86 @@ class studentRequest
         $DR_path);
 
     }
-    public static function Add($R_type,$R_position,$R_cost,$R_room,$R_sdate, $R_fdate)
+    public static function get2($R_type,$R_position,$R_cost,$R_room,$R_sdate, $R_fdate,$S_id,$C_id,$D_id)
+    {
+        
+        require("connect_connection.php");
+        $sql="SELECT Request.S_id,Request.R_id,Organization.O_id,Organization.O_name,AP_Request.AP_date,Request.R_status,Request.R_type,
+        Student.S_fname,Student.S_lname,  Request.R_position,Request.R_sdate,Request.R_fdate,Organization.O_addr,Colabor.C_fname,
+        Colabor.C_lname,Colabor.C_email,Colabor.C_tel,
+        Data_namedoc.D_fname,Data_namedoc.D_lname,
+        Data_namedoc.D_position,
+        Doc_Request.DR_path
+        
+        FROM Request LEFT JOIN AP_Request ON AP_Request.R_id = Request.R_id
+        INNER JOIN Colabor ON Colabor.C_id = Request.C_id
+        INNER JOIN Organization ON Colabor.O_id = Organization.O_id
+        INNER JOIN Student ON Student.S_id =Request.S_id
+        INNER JOIN Data_namedoc ON Data_namedoc.D_id = Request.D_id
+        LEFT JOIN Doc_Request ON Doc_Request.R_id = Request.R_id
+        WHERE Request.R_type='$R_type' AND  Request.R_position='$R_position'  AND  Request.R_cost='$R_cost'  AND  Request.R_room='$R_room'   AND  Request.R_sdate='$R_sdate'  AND  Request.R_fdate='$R_fdate'  AND  Request.S_id='$S_id'  AND  Request.C_id='$C_id' AND  Request.D_id='$D_id'";
+        $result=$conn->query($sql);
+        $my_row=$result->fetch_assoc();
+        
+        
+        $S_id = $my_row["S_id"];
+        $R_id = $my_row["R_id"];
+        $O_id = $my_row["O_id"];
+        $O_name = $my_row["O_name"];
+        $AP_date = $my_row["AP_date"];
+        $R_status = $my_row["R_status"];
+        $R_type = $my_row["R_type"];
+        $S_fname = $my_row["S_fname"];
+        $S_lname = $my_row["S_lname"];
+        $R_position = $my_row["R_position"];
+        $R_sdate = $my_row["R_sdate"];
+        $R_fdate = $my_row["R_fdate"];
+        $O_addr = $my_row["O_addr"];
+        $C_fname = $my_row["C_fname"];
+        $C_lname = $my_row["C_lname"];
+        $C_email = $my_row["C_email"];
+        $C_tel = $my_row["C_tel"];
+        $D_fname = $my_row["D_fname"];
+        $D_lname = $my_row["D_lname"];
+        $D_position = $my_row["D_position"];
+        $DR_path = $my_row["DR_path"];
+           
+        
+        require("connection_close.php");
+        return new studentRequest(      
+        $S_id,
+        $R_id,
+        $O_id,
+        $O_name,
+        $AP_date,
+        $R_status,
+        $R_type,
+        $S_fname,
+        $S_lname,
+        $R_position,
+        $R_sdate,
+        $R_fdate,
+        $O_addr,
+        $C_fname,
+        $C_lname,
+        $C_email,
+        $C_tel,
+        $D_fname,
+        $D_lname,
+        $D_position,
+        $DR_path);
+
+    }
+    public static function Add($R_type,$R_position,$R_cost,$R_room,$R_sdate, $R_fdate,$S_id,$C_id,$D_id)
 
     { 
 
        require("connect_connection.php");
 
-      
+     
 
-       $sql = "INSERT INTO `Request` (`R_type`, `R_position`, `R_cost`, `R_room`, `R_sdate`, `R_fdate`) VALUES ('$R_type', '$R_position', '$R_cost', '$R_room', '$R_sdate', '$R_fdate')";
+       $sql = "INSERT INTO `Request` (`R_id`, `R_type`, `R_position`, `R_cost`, `R_room`, `R_sdate`, `R_fdate`, `S_id`, `C_id`, `D_id`, `R_status`)
+        VALUES (NULL,'$R_type', '$R_position', '$R_cost', '$R_room', '$R_sdate', '$R_fdate', '$S_id', '$C_id', '$D_id','รอพิจารณา')";
 
        $result = $conn->query($sql);
 
