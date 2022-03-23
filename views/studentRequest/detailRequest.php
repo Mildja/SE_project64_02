624611ab387f4f4318a58e44b47f6f1c609c65a9
Namphueng <namphueng.k@ku.th>
2022-03-23 14:22:12 +0700

    Update detailRequest.php

--- views/studentRequest/detailRequest.php
+++ views/studentRequest/detailRequest.php
@@ -4,9 +4,8 @@
     <html>
 
     <head>
-        <link rel="stylesheet" href="views/studentRequest/styleteastudent.css">
+        <link rel="stylesheet" href="views/studentRequest/styletestudent.css">
     </head>
-
     <body bgcolor="#f0fff0">
         <center>
 
@@ -19,7 +18,7 @@
                     <div class="column">
                         <div class="box1">
 
-                  
+                 
                             <p5><?php echo "$studentRequest->R_type"; ?></p5><br/>
                             <p5><?php echo "ชื่อ $studentRequest->S_fname"; ?></p5>
                             <p5><?php echo "  $studentRequest->S_lname"; ?></p5><br/><br/>
@@ -36,17 +35,17 @@
                             <p5><?php echo "ชื่อ$studentRequest->C_fname"; ?></p5>
                             <p5><?php echo "  $studentRequest->C_lname"; ?></p5><br/>
                             <p5><?php echo "Email : $studentRequest->C_email"; ?></p5><br/>
-                            <p5><?php echo "เบอร์ติดต่อ : $studentRequest->C_tel"; ?></p5><br/>
+                            <p5><?php echo "เบอร์ติดต่อ : $studentRequest->C_tel"; ?></p5><br/><br/><br/>
                             
                             <p5><?php echo "ข้อมูลสถานการออกหนังสืออนุเคราะห์"; ?></p5><br/>
                             <p5><?php echo "ชื่อ $studentRequest->D_fname"; ?></p5>
                             <p5><?php echo "  $studentRequest->D_lname"; ?></p5><br/>
-                            <p5><?php echo "ตำแหน่ง $studentRequest->R_position"; ?></p5><br/>
+                            <p5><?php echo "ตำแหน่ง $studentRequest->D_position"; ?></p5><br/>
                             <p5><?php echo "สถานะ $studentRequest->R_status"; ?></p5><br/><br/><br/>
-                            <p5><?php echo "$studentRequest->DR_path"; ?></p5><br/><br/><br/>
-                            
+
+                            <?php echo '<a class="btn btn-primary btn-xs" href="data:application/pdf;base64,'.base64_encode($studentRequest->DR_path).' " onclick="debugBase64(this.href)"/>ดาวน์โหลด</a>';?>
                             <a type="button" href="?controller=studentRequest&action=index" class="btn btn-success">ย้อนกลับ</a>
-                           
+                     
                         </div>
                         
 
@@ -64,6 +63,10 @@
     </body>
     <br>
     <br>
- 
-
+ <script>
+function debugBase64(base64URL){
+    var win = window.open();
+    win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+}
+</script>
     </html>
