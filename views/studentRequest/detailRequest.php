5e22349f4c4504530951284b8dca2adfa5d2b915
njpor <nnppkk.j@gmail.com>
2022-03-25 06:25:20 +0700

    Update detailRequest.php

--- views/studentRequest/detailRequest.php
+++ views/studentRequest/detailRequest.php
@@ -9,9 +9,6 @@
     <body bgcolor="#f0fff0">
         <center>
 
-
-
-
             <?php echo $userid ?>
 
                
@@ -20,38 +17,36 @@
                             <center>
                             <br><h2><b><?php echo "รายละเอียดใบคำร้อง"; ?></b></h2><br><br>
                             </center>
-                            <p5><?php echo "รูปแบบคำร้อง: $studentRequest->R_type"; ?></p5><br>
-                            <p5><?php echo "ชื่อ $studentRequest->S_fname"; ?></p5>
-                            <p5><?php echo " นามสกุล  $studentRequest->S_lname"; ?></p5><br/><br>
-                            <p5><?php echo "รหัสนิสิต  $studentRequest->S_id"; ?></p5>
+                            <p5><?php echo "รูปแบบคำร้อง : $studentRequest->R_type"; ?></p5><br>
+                            <p5><?php echo "ชื่อ : $studentRequest->S_fname"; ?></p5>
+                            <p5><?php echo "  นามสกุล : $studentRequest->S_lname"; ?></p5>
+                            <p5><?php echo "  รหัสนิสิต : $studentRequest->S_id"; ?></p5><br><br>
                            
-                            <p5><?php echo "ยื่นฝึกงานตำแหน่ง: $studentRequest->R_position"; ?></p5><br>
-                           
-                            <p5><?php echo "วันเริ่มฝึกงาน : $studentRequest->R_sdate"; ?></p5><br>
+                            <p5><?php echo "ยื่นฝึกงานตำแหน่ง : $studentRequest->R_position"; ?></p5><br>
+
+                            <p5><?php echo "วันเริ่มต้นการฝึกงาน : $studentRequest->R_sdate"; ?></p5><br>
                             
-                            <p5><?php echo "วันสิ้นสุดฝึกงาน : $studentRequest->R_fdate"; ?></p5><br><br>
+                            <p5><?php echo "วันสิ้นสุดการฝึกงาน : $studentRequest->R_fdate"; ?></p5><br><br>
                             
                             <p5><?php echo "ข้อมูลสถานประกอบการ"; ?></p5><br>
-                            <p5><?php echo "$studentRequest->O_name"; ?></p5><br>
+                            <p5><?php echo "ชื่อบริษัท : $studentRequest->O_name"; ?></p5><br>
                             <p5><?php echo "ที่อยู่ : $studentRequest->O_addr"; ?></p5><br>
-                            <p5><?php echo "ชื่อ$studentRequest->C_fname"; ?></p5>
-                            <p5><?php echo "  $studentRequest->C_lname"; ?></p5><br>
+                            <p5><?php echo "ชื่อ HR : $studentRequest->C_fname"; ?></p5>
+                            <p5><?php echo "นามสกุล : $studentRequest->C_lname"; ?></p5><br>
                             <p5><?php echo "Email : $studentRequest->C_email"; ?></p5><br>
-                            <p5><?php echo "เบอร์ติดต่อ : $studentRequest->C_tel"; ?></p5><br><br><br>
+                            <p5><?php echo "เบอร์โทรติดต่อ : $studentRequest->C_tel"; ?></p5><br><br><br>
                             
-                            <p5><?php echo "ข้อมูลสถานการออกหนังสืออนุเคราะห์"; ?></p5><br/>
-                            <p5><?php echo "ชื่อ $studentRequest->D_fname"; ?></p5>
-                            <p5><?php echo "  $studentRequest->D_lname"; ?></p5><br/>
-                            <p5><?php echo "ตำแหน่ง $studentRequest->D_position"; ?></p5><br/>
+                            <p5><?php echo "ข้อมูลชื่อออกหนังสือขอความอนุเคราะห์"; ?></p5><br/>
+                            <p5><?php echo "ชื่อ : $studentRequest->D_fname"; ?></p5>
+                            <p5><?php echo "นามสกุล : $studentRequest->D_lname"; ?></p5><br/>
+                            <p5><?php echo "ตำแหน่ง : $studentRequest->D_position"; ?></p5><br><br><br>
 
                             <?php if ($studentRequest->R_status ==  "พิจารณาแล้ว" ) { 
-                                   echo "สถานะ $studentRequest->AP_approve"; 
+                                   echo "สถานะใบคำร้อง: $studentRequest->AP_approve"; 
                             }
                             else {
-
-                              echo "สถานะ $studentRequest->R_status";
-
-                            
+                              echo "สถานะใบคำร้อง:  $studentRequest->R_status";
+                        
                              } ?>
                           <br>
                           <br><br>
